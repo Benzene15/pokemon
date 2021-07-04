7f1419ea8fabfb486ce896d354c49e27bb11f371
Ben Cameron <camer343@umn.edu>
2021-07-03 21:03:25 -0500

    Changing model

--- pokemon.py
+++ pokemon.py
@@ -1,38 +1,51 @@
 from cv2 import cv2
 import numpy as np
 import os
+import os.path  
+from os import path
 import sys
 import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, BatchNormalization, MaxPool2D, Flatten, Dense, Dropout
 
 from sklearn.model_selection import train_test_split
 
-EPOCHS=10000
+EPOCHS=50
 IMG_WIDTH=30
 IMG_HEIGHT=30
 NUM_CATEGORIES=150
-TEST_SIZE=0.4
+TEST_SIZE=0.3
 
 def main():
+
+    #if(path.exists("pokemon.h5")):
+
+    #Load the image data
     images,labels,num=load_data("PokemonData")
+
+    #Split the data
     labels = tf.keras.utils.to_categorical(labels)
     x_train, x_test, y_train, y_test = train_test_split(
         np.array(images), np.array(labels), test_size=TEST_SIZE
     )
 
-    # Get a compiled neural network
+    #Make the model
     model = get_model()
 
-    # Fit model on training data
-    model.fit(x_train, y_train, epochs=EPOCHS)
+    #Train the model
+    model.fit(x_train, y_train, epochs=EPOCHS, batch_size=32)
 
-    # Evaluate neural network performance
+    #Evaluate the model
     model.evaluate(x_test,  y_test, verbose=2)
 
     print("n: "+ str(num))
+
+    #If there is a second arg in commond line save to it
+    #end file with .h5
     if(len(sys.argv)==2):
         filename = sys.argv[1]
         model.save(filename)
-        print("Model saved to "+filename+".")
+        print("Model saved to c"+filename+".")
 
 
 def load_data(data_dir):
@@ -55,17 +68,35 @@ def load_data(data_dir):
     return imgs, labels,n
 
 def get_model():
-    model= tf.keras.models.Sequential([
+    model = Sequential([
+                    Conv2D(filters=128,  kernel_size=(3,3), activation="sigmoid", input_shape=(IMG_WIDTH,IMG_WIDTH,3)),
+                    MaxPool2D(2,2, padding='same'),
+                    BatchNormalization(),
+                    Dropout(0.4),
+                    
+                    Conv2D(filters=256, kernel_size=(3,3), activation = "sigmoid"),
+                    MaxPool2D(2,2, padding="same"),
+                    BatchNormalization(),
+                    Dropout(0.4),
+                    
+                    Flatten(),
 
-        tf.keras.layers.Conv2D(32, (5,5), input_shape=(IMG_WIDTH,IMG_WIDTH,3)),
-        tf.keras.layers.MaxPooling2D(pool_size=(5,5)),
-        tf.keras.layers.Flatten(),
-        tf.keras.layers.Dropout(0.2),
+                    Dense(units = 4096, activation="relu", input_shape=(IMG_WIDTH,IMG_WIDTH,3)),
+                    Dropout(0.2),
 
+                    Dense(units = 2048, activation = "relu"),
+                    Dropout(0.2),
 
+                    Dense(units = 4096, activation="relu"),
+                    Dropout(0.2),
 
-        tf.keras.layers.Dense(NUM_CATEGORIES, activation="softmax")
-        
+                    Dense(units = 1024, activation = "relu"),
+                    Dropout(0.2),
+
+                    Dense(units = 512, activation = "relu"),
+                    Dropout(0.2),
+                    
+                    Dense(NUM_CATEGORIES, activation="softmax"),
     ])
 
     model.compile(
@@ -73,6 +104,9 @@ def get_model():
         loss="categorical_crossentropy",
         metrics=["accuracy"]
     )
+
+    print(model.summary())
+
     return model
 
 
